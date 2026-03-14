c68edd2189731bce9b9c3cc39b48a791ee6ac88e
Nytem <nytem@gmail.com>
2026-03-14 10:20:17 +0200

    feat: switched to the Gnav for the bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:journel_new/src/data/database/database_helper.dart';
 import 'package:journel_new/src/presentation/view/add_note_page.dart';
 import 'package:journel_new/src/presentation/view/calender_page.dart';
 import 'package:journel_new/src/presentation/view/folders_page.dart';
 import 'package:journel_new/src/presentation/view/home_page.dart';
+import 'package:journel_new/src/presentation/view/settings_page.dart';
 import 'package:journel_new/src/presentation/viewmodel/main_app_viewmodel.dart';
 //import 'package:sqflite/sqflite.dart';
 
@@ -21,6 +23,7 @@ class MyApp extends ConsumerWidget {
 
   static const List<Widget> _pages = [
     HomePage(),
+    AddNotePage(),
     CalenderPage(),
     FoldersPage(),
   ];
@@ -33,7 +36,24 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: _pages[currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
+        bottomNavigationBar: GNav(
+          gap: 4,
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 8, vertical: 20),
+          backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+          color: Colors.white,
+          activeColor: Colors.yellow,
+          textStyle: TextStyle(color: Colors.white),
+          rippleColor: Colors.yellowAccent,
+          onTabChange: (index) => navNotifier.changeTab(index),
+          tabs: [
+            GButton(icon: Icons.home_rounded, text: "Home"),
+            GButton(icon: Icons.add, text: "Add", iconSize: 30),
+            GButton(icon: Icons.calendar_month_rounded, text: "Calender"),
+            GButton(icon: Icons.folder_rounded, text: "Folders"),
+          ],
+        ),
+
+        /*bottomNavigationBar: BottomNavigationBar(
           currentIndex: currentIndex,
           selectedItemColor: Colors.yellow,
           backgroundColor: const Color.fromARGB(255, 19, 19, 19),
@@ -48,12 +68,17 @@ class MyApp extends ConsumerWidget {
               icon: Icon(Icons.calendar_month_rounded),
               label: 'calender',
             ),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Settings'),
             BottomNavigationBarItem(
               icon: Icon(Icons.folder_rounded),
               label: 'folders',
             ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_rounded),
+              label: 'Settings',
+            ),
           ],
-        ),
+        ),*/
       ),
 
       initialRoute: "/",
